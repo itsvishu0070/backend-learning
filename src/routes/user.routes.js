876be48b2eda81c8957dd5e14724be5b4e183eda
itsvishu0070 <panditvishu003@gmail.com>
2025-05-13 20:29:54 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { loginuser , registeruser , logoutuser, refreshaccesstoken } from "../controllers/user.controller.js";
+import { loginuser ,
+     registeruser ,
+     logoutuser,
+     refreshaccesstoken,
+     changecurrentpassword, 
+     getcurrentuser, 
+     updateaccountdetails, 
+     updateavatar,
+     updatecoverimage,
+     get_user_channel_profile,
+     get_watch_history } from "../controllers/user.controller.js";
 import { verifyjwt } from "../middlewares/auth.middleware.js";
 
 
@@ -25,5 +35,12 @@ router.route("/login").post(loginuser)
 //secured routes
 router.route("/logout").post(verifyjwt , logoutuser)
 router.route("/refresh-token").post(refreshaccesstoken)
+router.route("/change-password").post(verifyjwt , changecurrentpassword)
+router.route("/current-user").get(verifyjwt,getcurrentuser)
+router.route("/update-account").patch(verifyjwt,updateaccountdetails)
+router.route("/avatar").patch(verifyjwt,upload.single("avatar"),updateavatar)
+router.route("/cover-image").patch(verifyjwt,upload.single("/coverimage"),updatecoverimage)
+router.route("/c/:username").get(verifyjwt,get_user_channel_profile)
+router.route("/history").get(verifyjwt,get_watch_history)
 
 export default router 
